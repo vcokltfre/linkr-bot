68ce3fd3d30eda7194349e9274a3df5d59220b03
vcokltfre <vcokltfre@gmail.com>
2021-03-04 00:49:45 +0000

    feat: add ratelimit handling

--- src/backend/distributor.py
+++ src/backend/distributor.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from asyncio import Lock, sleep
 from loguru import logger
 
 from src.utils import Database
@@ -14,6 +15,8 @@ class Distributor:
         self.webhooks = defaultdict(list)
         self.channel_map = defaultdict(list)
         self.discord_channels = {}
+
+        self.locks = {}
 
         bot.loop.run_until_complete(self.saturate_cache())
 
@@ -45,7 +48,47 @@ class Distributor:
 
         data["username"] = data["username"] + f" | #{hex(id)[2:]}"
 
-        return await self.bot.http_session.post(self.pick_hook(channel_id), json=data)
+        hook = self.pick_hook(channel_id)
+        lock = self.locks.get(hook)
+        if not lock:
+            lock = Lock(loop=self.bot.loop)
+            self.locks[hook] = lock
+
+        if not hook.endswith("?wait=true"):
+            hook += "?wait=true"
+
+        await lock.acquire()
+        lastresp = None
+        for i in range(3):
+            response = lastresp = await self.bot.http_session.post(hook, json=data, headers={"X-RateLimit-Precision":"millisecond"})
+
+            headers = response.headers
+            status = response.status
+
+            reset_after = float(headers["X-RateLimit-Reset-After"])
+            if headers["X-RateLimit-Remaining"] == 0 and status != 429:
+                # Depleted the current ratelimit bucket
+                logger.warning(f"Ratelimit on channel {channel_id} has been depleted, it's lock will release after {reset_after}s.")
+                self.bot.loop.call_later(reset_after, lock.release())
+
+            if 200 <= status < 300:
+                lock.release()
+                return response
+
+            if status == 404:
+                logger.error(f"Received 404 while sending message {id} to channel {channel_id}.")
+                lock.release()
+                raise Exception() # TODO: Remove the webhook immediately, it doesn't exist anymore
+            elif status == 429:
+                if not headers.get("Via"):
+                    logger.error(f"Ratelimited by cloudflare when sending message {id} to channel {channel_id}")
+                    lock.release()
+                    raise Exception() # CF banned
+                logger.error(f"Ratelimited while sending message {id} to channel {channel_id}, sleeping for {reset_after}s")
+                await sleep(reset_after)
+
+        logger.error(f"Failed to send to channel {channel_id} on {id} 3 times: {await lastresp.text()}")
+        lock.release()
 
     async def broadcast(self, id: int, sender: int, channel: str, data: dict):
         if channel not in self.channel_map:
@@ -58,3 +101,5 @@ class Distributor:
                 continue
 
             await self.send(id, cid, data)
+
+        logger.info(f"Successfully broadcast message {id} on channel {channel} [Sender: {sender}]")
